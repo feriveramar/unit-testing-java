a6f761ca70c90c9dce7ee01ace0f5e133ae29a59
feriveramar <fernandaa.riveramartinez@gmail.com>
2024-10-27 20:31:57 -0600

    create branch develop

--- junit/CalculatorTest.java
+++ junit/CalculatorTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertEquals;
 public class CalculatorTest {
     @Test
     public void testAdd() {
-        Calculator calculator = new Calculator();
+        Calculator calculator = new Calculator(); 
         int result = calculator.add(2, 3);
         assertEquals(5, result);
     }
